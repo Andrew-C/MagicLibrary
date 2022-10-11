8ddf6f97c698d42b670ff9903e17216ae48fa407
Andrew <andrew12034@hotmail.com>
2022-10-11 03:00:04 -0700

    web scrapes card URL into database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,8 +12,21 @@ import org.jsoup.select.Elements;
 public class Main {
 
     public static void main(String[] args) {
+        // MongoDB access with environment variables
+        Dotenv dotenv = Dotenv.configure().load();
+        String uri = dotenv.get("MONGODB_PREFIX") + dotenv.get("MONGODB_USER") + ":" + dotenv.get("MONGODB_PASSWORD") + dotenv.get("MONGODB_CLUSTER");
+        MongoClientURI clientURI = new MongoClientURI(uri);
+        MongoClient mongoClient = new MongoClient(clientURI);
+
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("MagicLibrary");
+        MongoCollection urlCollection = mongoDatabase.getCollection("CardURL");
+
+        /*Document document = new Document("Name", "Andrew");
+        document.append("Sex", "Male");
+        urlCollection.insertOne(document);*/
+
         // scrape testing
-        /*final String url =
+        final String url =
                 "https://www.mtggoldfish.com/index/PLIST#paper";
         try {
             final org.jsoup.nodes.Document document = Jsoup.connect(url).get();
@@ -24,31 +37,22 @@ public class Main {
                 }
                 else {
                     Elements aElement = row.select("td a");
+                    // check that there is a link to a card and not just an empty box
                     String cardLink = aElement.attr("href");
                     if (cardLink.equals("")) {
                         continue;
                     }
-                    System.out.print("mtggoldfish.com" + cardLink + "  ");
+
+                    String cardURL = ("mtggoldfish.com" + cardLink);
                     String cardName = row.select("td:nth-of-type(1)").text();
-                    System.out.println(cardName);
+
+                    Document doc = new Document("Card Name", cardName).append("URL", cardURL);
+                    urlCollection.insertOne(doc);
                 }
             }
         }
         catch (Exception e) {
             e.printStackTrace();
-        }*/
-
-        // MongoDB access with environment variables
-        Dotenv dotenv = Dotenv.configure().load();
-        String uri = dotenv.get("MONGODB_PREFIX") + dotenv.get("MONGODB_USER") + ":" + dotenv.get("MONGODB_PASSWORD") + dotenv.get("MONGODB_CLUSTER");
-        MongoClientURI clientURI = new MongoClientURI(uri);
-        MongoClient mongoClient = new MongoClient(clientURI);
-
-        MongoDatabase mongoDatabase = mongoClient.getDatabase("MagicLibrary");
-        MongoCollection urlCollection = mongoDatabase.getCollection("CardURL");
-
-        Document document = new Document("Name", "Andrew");
-        document.append("Sex", "Male");
-        urlCollection.insertOne(document);
+        }
     }
 }
